daa3da7af1f18b79b1e5610a0d175078bb89ae87
yuyunaginagi <centrepg8@gmail.com>
2024-03-30 20:07:30 +0900

    ビューの編集と削除のエラー解消

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,6 +22,7 @@ class ReviewsController < ApplicationController
   def destroy
     review = Review.find(params[:id])
     review.destroy
+    redirect_to user_path(current_user)
   end
 
   private
