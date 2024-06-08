5156ad61400947e0c38741e00caf97ffdbfe30d3
yuyunaginagi <centrepg8@gmail.com>
2024-06-08 23:46:38 +0900

    ビューの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
-  resources :results, only: [:new, :create, :destroy]
-  resources :users,   only: [:index, :show, :edit, :update]
-  resources :reviews, only: [:new, :create, :show, :destroy] do 
+  resources   :results,  only: [:new, :create, :destroy]
+  resources   :users,    only: [:index, :show, :edit, :update]
+  resources   :reviews,  only: [:new, :create, :show, :destroy] do 
     resources :comments, only: [:create, :destroy]
-    resources :likes, only: [:create, :destroy]
+    resources :likes,    only: [:create, :destroy]
   end
   
 end
